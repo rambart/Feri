d75bd2b3812a0b32a654bb0f16a9e066e5d064b5
Thomas Loring <TomLoringDev@gmail.com>
2019-07-09 17:00:54 -0700

    Initial Commit

---
+++ Wrath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wrath
+//
+//  Created by Thomas Loring on 7/9/19.
+//  Copyright © 2019 Thomas Loring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
